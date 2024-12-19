c9ecf921ecb8dce65efd3faafb75bec9636f95b1
churroTheDuck <152838803+churroTheDuck@users.noreply.github.com>
2024-12-19 10:39:21 -0800

    Added end screen

--- sketch.js
+++ sketch.js
@@ -4,6 +4,7 @@ var seaLevel = [0];
 var seaHeight = 0;
 var i = 0;
 let yoff = 0;
+var lineHeight = 600;
 
 function preload() {
     table = loadTable("sea.csv", "header");
@@ -21,7 +22,7 @@ function draw() {
 
     if (i < rows.length) {
         seaHeight = lerp(seaHeight, parseFloat(rows[i].get("Value")) * 5, 0.1);
-        i++;
+        i += 2;
     }
 
     push();
@@ -33,7 +34,9 @@ function draw() {
     for (let x = 0; x <= width; x += 10) {
         let y = map(noise(xoff, yoff), 0, 1, -50, 50);
         vertex(x, -seaHeight + y + 600);
-        xoff += 0.01;
+        if (i < rows.length - 5) {
+            xoff += 0.01;
+        }
     }
     let y = map(noise(xoff, yoff), 0, 1, -50, 50);
     vertex(width, -seaHeight + y + 600);
@@ -41,7 +44,9 @@ function draw() {
     endShape();
     pop();
 
-    yoff += 0.08;
+    if (i < rows.length - 5) {
+        yoff += 0.08;
+    }
 
     push();
     if (i == rows.length) {
@@ -63,7 +68,7 @@ function draw() {
         textSize(50);
         textFont(lato);
         textAlign(LEFT, CENTER);
-        text(60 - (i / 10) + "mm", width - 200, i);
+        text(120 - (i / 5) + "mm", width - 200, i);
     }
     pop();
 
@@ -72,16 +77,26 @@ function draw() {
     noStroke();
     textSize(50);
     textFont(lato);
-    textAlign(LEFT, CENTER);
-    text(i < rows.length ? rows[i].get("Date") : rows[i - 1].get("Date"), 200, -seaHeight + 600);
+    textAlign(RIGHT, CENTER);
+    text(i < rows.length ? rows[i].get("Date").slice(7) : rows[i - 1].get("Date").slice(7), 200, -seaHeight + 600);
     textAlign(RIGHT, CENTER);
     text("1992", 200, 600);
+    pop();
+
     if (i >= rows.length) {
-        text("2024", 200, -seaHeight + 600);
-        stroke(255, 255, 255, 80);
+        push();
+        stroke("FF0000");
         strokeWeight(5);
         noFill();
-        line(250, -seaHeight + 600, width - 250, -seaHeight + 600);
+        lineHeight = lerp(lineHeight, -seaHeight + 600, 0.1);
+        line(250, 600, 250, lineHeight);
+        pop();
+        push();
+        noStroke();
+        fill("#FFFFFF");
+        textSize(50);
+        textFont(lato);
+        textAlign(CENTER, CENTER);
+        text("Sea levels are rising every year.", width / 2, height / 2)
     }
-    pop();
 }
